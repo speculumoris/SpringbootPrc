a16291f086cc4a5f04017d3d185caffa266fc9eb
speculumoris <sefa.byndr4@gmail.com>
2023-01-17 10:49:22 +0300

    firstSpringPrc

---
+++ src/main/java/com/tpe/SpringBootPrcApplication.java
@@ -0,0 +1,28 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrcApplication.class, args);
+	}
+
+}
+/*
+Katmanları oluşturalım.
+ÖDEV:)
+1-Customer classı oluştur
+2-fieldlar
+    private Long id->otomatik identity yöntemiyle oluşturulsun
+    private String name->Null olmamalı, space olmamalı, en az 2 en fazla 50 karakter
+    private String lastName->Null olmamalı, space olmamalı, en az 2 en fazla 50 karakter
+    private String email->geçerli bir email olmalı
+    private String phone
+3-Gerekli annotationları ekle
+    Getter-Setter metodları oluştur
+    Parametreli(tüm fieldlar) constructor oluştur
+    Parametresiz constructor oluştur
+ */
